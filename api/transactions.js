d1b7665abd8adb37bb0a8c62fb93b987856b4af6
alanduarte90 <alanduarte90@gmail.com>
2019-05-01 11:02:49 -0300

    transactions: include id on getByID

--- api/transactions.js
+++ api/transactions.js
@@ -66,7 +66,7 @@ module.exports = app => {
 	
 	const getById = (req, res) => {
 		app.db('transactions')
-			.select('type', 'transaction', 'description', 'amount', 'transactionDate')
+			.select('id', 'type', 'transaction', 'description', 'amount', 'transactionDate')
 			.where({ userId: req.params.userId })
 			.then(transactions => res.json(transactions))
 			.catch(err => res.status(500).send(err))
