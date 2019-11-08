fb49378fad331d403f3db65b8c0202facc11662c
alanduarte90 <alanduarte90@gmail.com>
2019-11-08 13:30:29 +0000

    Change final route

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,7 @@
 module.exports = app => {
 
 	app.use(function(req, res, next) {
-		res.header("Access-Control-Allow-Origin", "https://controlle.herokuapp.com/"); // update to match the domain you will make the request from
+		res.header("Access-Control-Allow-Origin", "https://controlle.herokuapp.com"); // update to match the domain you will make the request from
   		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   		next();
 	})
