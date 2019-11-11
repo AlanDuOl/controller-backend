f648bc67ee16bb9987ca59c0acc7e2714a62dcda
alanduarte90 <alanduarte90@gmail.com>
2019-11-11 15:30:32 +0000

    Hard coded connection from heroku postgre addon

--- knexfile.js
+++ knexfile.js
@@ -35,8 +35,8 @@ module.exports = {
         client: 'pg',
         connection: {
             host: 'ec2-54-75-249-16.eu-west-1.compute.amazonaws.com',
-            user: 'postgres',
-            password: '123',
+            user: 'yalbfainbznvmz',
+            password: 'f74bba1252f3e5d7bfbead10cb57f294143e2538f600a764408086dc506e8e70',
             database: 'postgres://yalbfainbznvmz:f74bba1252f3e5d7bfbead10cb57f294143e2538f600a764408086dc506e8e70@ec2-54-75-249-16.eu-west-1.compute.amazonaws.com:5432/dat6vkuviocndi'
 
         },
