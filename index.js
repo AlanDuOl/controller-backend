4ca82ae82d385ac9531efdac6756d855e510cd33
alanduarte90 <alanduarte90@gmail.com>
2019-11-12 07:12:09 +0000

    Change production port

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ consign()
     .then('./config/routes.js')
     .into(app)
 
-app.listen(process.env.NODE_ENV === 'production' ? process.env.DB_PRD_PORT : process.env.DB_DEV_PORT, () => {
+app.listen(process.env.NODE_ENV === 'production' ? process.env.PORT : process.env.DB_DEV_PORT, () => {
     console.log('Server running...')
 })
