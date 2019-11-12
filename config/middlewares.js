c10a7973e3a38393ab99b6651f4d574b45eae1b8
alanduarte90 <alanduarte90@gmail.com>
2019-11-12 00:30:35 +0000

    Update cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,17 @@
 const bodyParser = require('body-parser')
-const cors = require('cors')
+// const cors = require('cors')
 
-const origin = {
-    origin: 'https://controlle.herokuapp.com'
-}
+// const origin = {
+//     origin: 'https://controlle.herokuapp.com'
+// }
 
 
 module.exports = app => {
     app.use(bodyParser.json())
-    app.use(cors(origin))
+    // app.use(cors(origin))
+    app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "https://controlle.herokuapp.com"); // update to match the domain you will make the request from
+  		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  		next();
+	})
 }
