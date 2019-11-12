65e420a6eab73c9c905228418689d838fae02e28
alanduarte90 <alanduarte90@gmail.com>
2019-11-12 00:06:47 +0000

    Add cors origin object

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,12 @@
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const origin = {
+    origin: 'https://controlle.herokuapp.com'
+}
+
 
 module.exports = app => {
     app.use(bodyParser.json())
-    app.use(cors())
+    app.use(cors(origin))
 }
