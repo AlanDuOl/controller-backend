db2f89704a8fec4a7f8ac63261fde3c6beab9b39
alanduarte90 <alanduarte90@gmail.com>
2019-11-12 21:27:59 +0000

    Set header response methods

--- config/middlewares.js
+++ config/middlewares.js
@@ -11,7 +11,8 @@ module.exports = app => {
     // app.use(cors(origin))
     app.use(function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "https://controlle.herokuapp.com"); // update to match the domain you will make the request from
-  		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   		next();
 	})
 }
