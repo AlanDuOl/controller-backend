2018dfea59b284ea4e84bdca07ce7c5d2d4517b2
alanduarte90 <alanduarte90@gmail.com>
2019-11-12 07:32:41 +0000

    Add a try/catch to signin on auth.js

--- api/auth.js
+++ api/auth.js
@@ -4,37 +4,40 @@ const bcrypt = require('bcrypt-nodejs')
 
 module.exports = app => {
     const signin = async (req, res) => {
-        if (!req.body.email || !req.body.password) {
-            return res.status(400).send('Informe usuário e senha!')
+        try {
+            if (!req.body.email || !req.body.password) {
+                return res.status(400).send('Informe usuário e senha!')
+            }
+    
+            const user = await app.db('users')
+                .where({ email: req.body.email })
+                .first()
+    
+            if (!user) return res.status(400).send('Usuário não encontrado!')
+    
+            const isMatch = bcrypt.compareSync(req.body.password, user.password)
+            if (!isMatch) return res.status(401).send('Email/Senha inválidos!')
+    
+            const now = Math.floor(Date.now() / 1000)
+    
+            const payload = {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                iat: now,
+                exp: now + (60 * 60 * 8 * 1)
+            }
+    
+            res.json({
+                ...payload,
+                token: jwt.encode(payload, process.env.authSecret)
+            })
+        }  catch(e) {
+            console.log(e)
         }
-
-        const user = await app.db('users')
-            .where({ email: req.body.email })
-            .first()
-
-        if (!user) return res.status(400).send('Usuário não encontrado!')
-
-        const isMatch = bcrypt.compareSync(req.body.password, user.password)
-        if (!isMatch) return res.status(401).send('Email/Senha inválidos!')
-
-        const now = Math.floor(Date.now() / 1000)
-
-        const payload = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            iat: now,
-            exp: now + (60 * 60 * 8 * 1)
-        }
-
-        res.json({
-            ...payload,
-            token: jwt.encode(payload, process.env.authSecret)
-        })
     }
 
     const validateToken = async (req, res) => {
-        console.log(req.body)
         const userData = req.body || null
         try {
             if(userData) {
